489f1a965c227b164eae063d7bd01b3380bd0367
Ed Holmes <ed.holmes@me.com>
2017-02-14 09:34:36 +0000

    Added previous work

---
+++ sensorTest1/sensorTest1.ino
@@ -0,0 +1,31 @@
+#include <PololuQTRSensors.h>
+#include <QTRSensors.h>
+
+#define NUM_SENSORS   3     // number of sensors used
+#define TIMEOUT       2500  // waits for 2500 us for sensor 
+
+
+QTRSensorsRC qtrrc((unsigned char[]) {10,11,12},  NUM_SENSORS, 
+                          TIMEOUT, QTR_NO_EMITTER_PIN); 
+unsigned int sensorValues[NUM_SENSORS];
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop()
+{
+  qtrrc.read(sensorValues, QTR_NO_EMITTER_PIN);
+
+  
+  unsigned char i;
+  for (i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+  delay(250);
+}
